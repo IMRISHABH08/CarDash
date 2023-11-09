7b8ae54636e055e5a9af639126c03cde014ec879
Rishabh Gupta <Rishabh@Apples-MBP.alphionsee.in>
2023-11-10 00:46:54 +0530

    Design System Created

---
+++ lib/global/design_system/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Indra {
+  const Indra._();
+
+  static const lightBlue = Color(0xff339AF0);
+
+  static const white = Colors.white;
+  
+}
---
+++ lib/global/design_system/styles/sizing.dart
@@ -0,0 +1,17 @@
+class SpaceX {
+  const SpaceX._();
+
+  static const px10 = 10.0;
+
+  static const px20 = 20.0;
+
+  static const px30 = 30.0;
+
+  static const px40 = 40.0;
+
+  static const px80 = 80.0;
+
+  static const px100 = 100.0;
+
+  static const px120 = 120.0;
+}
